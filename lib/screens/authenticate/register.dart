5a09f48983f3890e954808f2bf09b0c293378e93
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-06-05 12:31:43 +0530

    init and implement register screen

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,68 @@
+import 'package:cricket_users_app/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  // get AuthService instance
+  final AuthService _authService = AuthService();
+
+  // text field state
+  String _email = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        // backgroundColor: Colors.brown[400],
+        // elevation: 0.0,
+        title: Text('Sign Up'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                onChanged: (value) {
+                  setState(() => _email = value);
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() => _password = value);
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                child: Text('Register'),
+                onPressed: () async {
+                  print(_email);
+                  print(_password);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
