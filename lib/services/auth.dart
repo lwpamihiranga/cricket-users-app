6912fa9d6da09e5c457958f0b0a7d376c26474fe
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-06-05 12:09:06 +0530

    init and implement AuthService

---
+++ lib/services/auth.dart
@@ -0,0 +1,48 @@
+import 'package:cricket_users_app/models/user.dart' as Model;
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create User obj based on Firebase User
+  Model.User? _userFromFirebaseUser(User user) {
+    // ignore: unnecessary_null_comparison
+    if (user != null) {
+      return Model.User(uid: user.uid);
+    } else {
+      return null;
+    }
+  }
+
+  // auth change user stream
+  Stream<Model.User?> get user {
+    return _auth
+        .authStateChanges()
+        .map((User? user) => _userFromFirebaseUser(user!));
+  }
+
+  // sign in anonymously
+  Future signInAnonymously() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+  // sing in with email and password
+
+  // register with email and password
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
