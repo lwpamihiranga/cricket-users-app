082084143beacc849d72404ba8ad171ea5faa83f
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-06-05 12:31:58 +0530

    update authenticate screen

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,4 +1,4 @@
-import 'package:cricket_users_app/screens/authenticate/sign_in.dart';
+import 'package:cricket_users_app/screens/authenticate/register.dart';
 import 'package:flutter/material.dart';
 
 class Authenticate extends StatefulWidget {
@@ -12,7 +12,7 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SignIn(),
+      child: Register(),
     );
   }
 }
