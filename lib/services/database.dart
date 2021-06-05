6dfee79c39b9b00ae0862bac599cdc3e8d12e37d
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-06-05 17:32:08 +0530

    update database service

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,15 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  final String uid;
-  DatabaseService({required this.uid});
-
   // collection reference
   final CollectionReference playerCollection =
       FirebaseFirestore.instance.collection('players');
 
-  Future updateData(String name, String age) async {
-    return await playerCollection.doc(uid).set({'name': name, 'age': age});
+  Future addPlayer(
+      String bio, String name, String age, String country, String runs) async {
+    return await playerCollection.doc().set({
+      'bio': bio,
+      'name': name,
+      'age': age,
+      'country': country,
+      'runs': runs
+    });
+  }
+
+  Future updatePlayer(String player, String bio, String name, String age,
+      String country, String runs) async {
+    return await playerCollection.doc(player).update({
+      'bio': bio,
+      'name': name,
+      'age': age,
+      'country': country,
+      'runs': runs
+    });
+  }
+
+  Future deletePlayer(String player) async {
+    return await playerCollection.doc(player).delete();
   }
 
   // get player stream
