6aa950d5415ae5f0ebd0031e5feb96682315b19c
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-06-05 14:01:34 +0530

    init loading widget

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:cricket_users_app/services/auth.dart';
+import 'package:cricket_users_app/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -14,6 +15,8 @@ class _RegisterState extends State<Register> {
   final AuthService _authService = AuthService();
   // form key
   final _formKey = GlobalKey<FormState>();
+
+  bool _loading = false;
 
   // text field state
   String _email = '';
@@ -38,73 +41,80 @@ class _RegisterState extends State<Register> {
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration:
-                    InputDecoration(hintText: 'Email', labelText: 'Email'),
-                // validator: (value) => value!.isEmpty ? 'Enter an email' : null,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter an email';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() => _email = value);
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Password', labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value!.length < 6) {
-                    return 'Password should be at least 6 characters long ';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() => _password = value);
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(
-                child: Text('Register'),
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result =
-                        await _authService.register(_email, _password);
+      body: _loading
+          ? Loading()
+          : Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Email', labelText: 'Email'),
+                      // validator: (value) => value!.isEmpty ? 'Enter an email' : null,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter an email';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() => _email = value);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Password', labelText: 'Password'),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.length < 6) {
+                          return 'Password should be at least 6 characters long ';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() => _password = value);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      child: Text('Register'),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() => _loading = true);
 
-                    if (result == null) {
-                      setState(() => _error = 'Invalid Email or Password!');
-                    }
-                  }
-                },
+                          dynamic result =
+                              await _authService.register(_email, _password);
+
+                          if (result == null) {
+                            setState(() {
+                              _loading = false;
+                              _error = 'Invalid Email or Password!';
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      _error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                _error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:cricket_users_app/services/auth.dart';
+import 'package:cricket_users_app/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -14,6 +15,8 @@ class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
   // form key
   final _formKey = GlobalKey<FormState>();
+
+  bool _loading = false;
 
   // text field state
   String _email = '';
@@ -38,87 +41,94 @@ class _SignInState extends State<SignIn> {
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration:
-                    InputDecoration(hintText: 'Email', labelText: 'Email'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter an email';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() => _email = value);
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Password', labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value!.length < 6) {
-                    return 'Password should be at least 6 characters long ';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() => _password = value);
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(
-                child: Text('Sign in'),
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result =
-                        await _authService.signIn(_email, _password);
+      body: _loading
+          ? Loading()
+          : Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Email', labelText: 'Email'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter an email';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() => _email = value);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Password', labelText: 'Password'),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.length < 6) {
+                          return 'Password should be at least 6 characters long ';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() => _password = value);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      child: Text('Sign in'),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() => _loading = true);
 
-                    if (result == null) {
-                      setState(() => _error = 'Invalid Email or Password!');
-                    }
-                  }
-                },
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                _error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-            ],
-          ),
-        ),
+                          dynamic result =
+                              await _authService.signIn(_email, _password);
 
-        // Old anonymous sign in button
-        // ElevatedButton(
-        //   child: Text('Sign In Anonymously'),
-        //   onPressed: () async {
-        //     dynamic result = await _authService.signInAnonymously();
-        //
-        //     if (result == null) {
-        //       print('Error Signing In');
-        //     } else {
-        //       print('Signed In');
-        //       print(result.uid);
-        //     }
-        //   },
-        // ),
-      ),
+                          if (result == null) {
+                            setState(() {
+                              _loading = false;
+                              _error = 'Invalid Email or Password!';
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      _error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Old anonymous sign in button
+              // ElevatedButton(
+              //   child: Text('Sign In Anonymously'),
+              //   onPressed: () async {
+              //     dynamic result = await _authService.signInAnonymously();
+              //
+              //     if (result == null) {
+              //       print('Error Signing In');
+              //     } else {
+              //       print('Signed In');
+              //       print(result.uid);
+              //     }
+              //   },
+              // ),
+            ),
     );
   }
 }
