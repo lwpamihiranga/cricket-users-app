f7a18e330ec72d12b47488d723be6d3ad8dabc21
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-06-05 22:27:49 +0530

    update home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,18 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cricket_users_app/screens/player/view.dart';
 import 'package:cricket_users_app/services/auth.dart';
+import 'package:cricket_users_app/services/database.dart';
+import 'package:cricket_users_app/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
 
   final AuthService _authService = AuthService();
+  final DatabaseService _databaseService = DatabaseService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.brown[50],
       appBar: AppBar(
         title: Text('Cricket App'),
-        // backgroundColor: Colors.brown[400],
-        // elevation: 0.0,
         actions: <Widget>[
           ElevatedButton.icon(
             icon: Icon(Icons.person),
@@ -22,6 +25,71 @@ class Home extends StatelessWidget {
             },
           ),
         ],
+      ),
+      body: StreamBuilder(
+        stream: _databaseService.players,
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.data != null) {
+            return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  // return Text(streamSnapshot.data!.docs[index]['name']);
+                  return Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Card(
+                      margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 25.0,
+                          backgroundColor: Colors.lightBlueAccent,
+                        ),
+                        title: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(streamSnapshot.data!.docs[index]['name']),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(streamSnapshot.data!.docs[index]
+                                    ['country']),
+                              ],
+                            )
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ViewPlayer(
+                                id: streamSnapshot.data!.docs[index].id,
+                                bio: streamSnapshot.data!.docs[index]['bio'],
+                                name: streamSnapshot.data!.docs[index]['name'],
+                                age: streamSnapshot.data!.docs[index]['age'],
+                                country: streamSnapshot.data!.docs[index]
+                                    ['country'],
+                                runs: streamSnapshot.data!.docs[index]['runs'],
+                              ),
+                            ),
+                          );
+                          // _databaseService.deletePlayer(
+                          //     streamSnapshot.data!.docs[index].id);
+                        },
+                      ),
+                    ),
+                  );
+                });
+          } else {
+            return Loading();
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.pushNamed(context, '/add');
+        },
       ),
     );
   }
