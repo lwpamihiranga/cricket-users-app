6080073657244eaed6d6b6da776c0195186de3ce
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-06-05 22:43:17 +0530

    remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:cricket_users_app/screens/player/add.dart';
-import 'package:cricket_users_app/screens/player/view.dart';
 import 'package:cricket_users_app/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
